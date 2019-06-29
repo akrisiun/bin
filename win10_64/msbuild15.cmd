405115b75f9ea32e01882f6a75d1140729e61f1e
AndriusK <akrisiun@gmail.com>
2019-06-29 09:48:42 +0300

    msbuild + git batches

--- win10_64/msbuild15.cmd
+++ win10_64/msbuild15.cmd
@@ -1,8 +1,5 @@
 
-@REM C:\Program Files (x86)\Microsoft Visual Studio\Preview
-"d:\Data\ms-vstudio\Preview\MSBuild\15.0\Bin\MSBuild.exe" "%1" %2 %3 /verbosity:m
+@REM "c:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" %* /verbosity:m
+@REM "d:\Data\ms-vstudio\Preview\MSBuild\15.0\Bin\MSBuild.exe" "%1" %2 %3 /verbosity:m
 
-@REM "C:\Program Files (x86)\Microsoft Visual Studio\Preview\Community\MSBuild\15.0\Bin\MSBuild.exe" "%1" %2 %3 /verbosity:m
-
-@REM # "c:\bin\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\MSBuild.exe" "%1" %2 %3 /verbosity:m
-@REM "c:\Program Files (x86)\MSBuild\15.0\Bin\MSBuild.exe" "%1" %2 %3 /verbosity:m
+"c:\bin\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin\MSBuild.exe" %* /verbosity:m
