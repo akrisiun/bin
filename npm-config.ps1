0492dacda56a93f0747f69ba3b06621137251ddc
AndriusK <andrius.k@sanitex.eu>
2019-08-19 07:57:54 +0300

    include serve-{port},  DejaVu ttf binaries

---
+++ npm-config.ps1
@@ -0,0 +1,28 @@
+# & "C:\Program Files\nodejs\npm.CMD" add -g @vue/cli
+
+vue info
+write-host npm get registry
+npm get registry
+write-host npm config get proxy
+npm config get proxy
+write-host http-proxy
+npm config get http-proxy
+write-host https-proxy
+npm config get https-proxy
+
+write-host npm config ls -l
+npm config ls -l
+
+npm config get proxy
+
+write-host "node -v"
+node -v
+write-host "npm -v"
+npm -v
+write-host "yarn -v"
+yarn -v
+write-host "vue -V (vue-cli)"
+vue -V
+
+write-host "~\.vue-templates"
+ls ~\.vue-templates
