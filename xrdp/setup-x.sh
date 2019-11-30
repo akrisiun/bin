57b6f568b5576ad9cd1541c64b04d98328c7204c
Andrius-K <akrisiun@gmail.com>
2019-11-30 13:48:05 +0200

    for Debian setup X11

---
+++ xrdp/setup-x.sh
@@ -0,0 +1,15 @@
+!/bin/bash
+#
+# git clone https://github.com/akrisiun/bin
+
+#  mc
+su apt-get update
+# su root
+#  visudo
+sudo apt-get update
+# runlevel
+
+sudo apt install -y lightdm
+sudo lightdm --show-config
+sudo lightdm --test-mode --debug
+
