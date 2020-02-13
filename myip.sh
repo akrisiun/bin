59520a550a3b05741def36a77e8ec1b029c2e1d1
Andrius-K <akrisiun@gmail.com>
2020-02-13 06:34:07 +0200

    my ip be $OSTYPE

--- myip.sh
+++ myip.sh
@@ -20,16 +20,34 @@ if [ $n -eq 8 ]; then
    echo "~/bin/myip.sh:"
 fi
 
+echo "\$OSTYPE= $OSTYPE"
 printf "\n My external IP address: \e[1m"
 curl -s http://ipecho.net/plain
 
 # https://misc.flogisoft.com/bash/tip_colors_and_formatting
 printf "\n\e[36m ip route interface: "
 ip route get 8.8.8.8 2>/dev/null| awk '{print $5}'
-printf "\r \e[92mip addr show eth0\e[37m\n"
-ip addr show eth0 | grep inet
-printf "\e[35m "
-ip addr show en0 | grep inet
+
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    # ...
+    printf "\r \e[92mip addr show eth0\e[37m\n"
+    ip addr show eth0 | grep inet
+elif [[ "$OSTYPE" == "darwin18" ]]; then
+    # Mac OSX
+    printf "\e[35m ifconfig en0\e[37m\n"
+    # ip addr show 
+    ifconfig en0 | grep inet
+#elif [[ "$OSTYPE" == "cygwin" ]]; then
+#    # POSIX compatibility layer and Linux environment emulation for Windows
+#elif [[ "$OSTYPE" == "msys" ]]; then
+#    # Lightweight shell and GNU utilities compiled for Windows (part of MinGW)
+elif [[ "$OSTYPE" == "win32" ]]; then
+    # I'm not sure this can happen: Windows
+    ifconfig /all
+else    
+    printf "\e[35m Uknown IP"
+fi
+
 
 # colors prinft:  [34 - blue, 31-red   35m - Magenta  92 -lt green  37m - lt gray
 # \e[0m  reset  \e1m - bold
